ff1226e29690022a6869ed6e45748f0fd01ab7f7
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2022-06-19 21:53:44 -0700

    Initial maven multimodule structure

---
+++ datasource-micrometer-spring-boot/src/main/java/net/ttddyy/observation/App.java
@@ -0,0 +1,13 @@
+package net.ttddyy.observation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ datasource-micrometer/src/test/java/net/ttddyy/observation/AppTest.java
@@ -0,0 +1,20 @@
+package net.ttddyy.observation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
